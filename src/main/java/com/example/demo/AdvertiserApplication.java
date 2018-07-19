3a07652b8d71a4a46559847578df2567495b8b1f
Jeff Jarin <jjarin@pinsightmedia.com>
2018-07-19 16:06:37 -0500

    Added Proper Exception Handler

--- src/main/java/com/example/demo/AdvertiserApplication.java
+++ src/main/java/com/example/demo/AdvertiserApplication.java
@@ -4,28 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdvertiserApplication /*implements  CommandLineRunner */{
-
-//	private Logger logger = LoggerFactory.getLogger(this.getClass());
-//
-//	@Autowired
-//	AdvertiserMapper myMapper;
+public class AdvertiserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdvertiserApplication.class, args);
 	}
-
-	/*@Override
-	public void run(String... args) throws Exception {
-
-		logger.info("Advertiser id 10001 -> {}", myMapper.findById(10001));
-
-		logger.info("Inserting -> {}", myMapper.insert(new Advertiser(10010, "Inmobi", "Will Smith", 10000)));
-
-		logger.info("Update 10003 -> {}", myMapper.update(new Advertiser(10001, "Pinsight", "John Johnson", 30000)));
-
-		myMapper.deleteById(10002);
-
-		logger.info("All Advertisers -> {}", myMapper.findAll());
-	}*/
 }
--- src/main/java/com/example/demo/controller/AdvertiserController.java
+++ src/main/java/com/example/demo/controller/AdvertiserController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
+import com.example.demo.Exception.ResourceNotFoundException;
 import com.example.demo.model.Advertiser;
 import com.example.demo.repository.AdvertiserMapper;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,21 +31,47 @@ public class AdvertiserController {
 
   // get a single Advertiser by Id
   @GetMapping("/{id}")
-  public Advertiser getAdvertiserById(@PathVariable(value = "id") int id) {
-    return mapper.findById(id);
+  public ResponseEntity<Advertiser> getAdvertiserById(@PathVariable(value = "id") int id) {
+    Advertiser advertiser = mapper.findById(id);
+    if(advertiser == null)
+      throw new ResourceNotFoundException(" Advertiser ", " Id ", id );
+    return new ResponseEntity<Advertiser>(advertiser, HttpStatus.OK);
+  }
+
+  // get a single Advertiser by Id and Validate if Valid to do Transaction
+  @GetMapping("/validate/{id}")
+  public ResponseEntity<?> getAdvertiserByIdAndValidateEligibility(@PathVariable(value = "id") int id) {
+    Advertiser advertiser = mapper.findById(id);
+    if(!validateCreditLimit(id)){
+      throw new ResourceNotFoundException("Advertiser Not Allowed to transact ", " Id ", id );
+    }
+
+    return new ResponseEntity<Advertiser>(advertiser, HttpStatus.OK);
   }
 
   // Update Advertiser
-  @PutMapping
+  @PutMapping("/")
   public int updateAdvertiser(@Valid @RequestBody Advertiser advertiser){
     return mapper.update(advertiser);
   }
 
   // Delete Advertiser
   @DeleteMapping("/{id}")
-  public int deleteAdvertiser(@PathVariable(value = "id") int id) {
+  public int  deleteAdvertiser(@PathVariable(value = "id") int id) {
     return mapper.deleteById(id);
   }
 
 
+  /**
+   * Verify and Validate Advertiser
+   *
+   * @param id
+   */
+  private boolean validateCreditLimit(int id) {
+    Advertiser advertiser = mapper.findById(id);
+    if(advertiser != null &&  advertiser.getCreditLimit() > 0){
+      return true;
+    }
+    return false;
+  }
 }
